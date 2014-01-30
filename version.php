341b3d8dab1f322e17e710c64362505423b6eace
Ronaldo Amaral <rsantos@iff.edu.br>
2014-01-29 23:46:02 -0200

    modificando versão

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013012902;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013012903;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->component = 'repository_semantic_lo_url'; // Full name of the plugin (used for diagnostics)
 $plugin->cron = 0; // How often to run automatic updates (0 to disable)
 $plugin->release = '0.1';
